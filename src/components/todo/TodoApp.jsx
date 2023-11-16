d39b56b1ebd637d784bdbb35eef923c4931d8eea
May Pyae Sone Nyein Win <nyeinwin04@gmail.com>
2023-11-16 14:57:44 +0630

    After adding App.css at App.js

---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class TodoApp extends Component{
+    render(){
+        return(
+            <div className="TodoApp">
+            My todo application
+            </div>
+        );
+    }
+}
+
+export default TodoApp;
