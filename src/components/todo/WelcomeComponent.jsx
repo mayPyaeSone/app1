985ee6ad640b92d1efc993df5533ba7ef15aa8ca
May Pyae Sone Nyein Win <nyeinwin04@gmail.com>
2024-01-31 23:12:57 +0630

    After modified welcome message

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -8,6 +8,10 @@ class WelcomeComponent extends Component{
             <> 
                 <h1>Welcome</h1>
                 <div className="container">Welcome {this.props.params.name}. You can manage your todos <Link to="/todos">here</Link></div> 
+                <div className="container">
+                Click here to get customized Welcome message.
+                <button onClick={this.retriveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div> 
             </>
            
             )
